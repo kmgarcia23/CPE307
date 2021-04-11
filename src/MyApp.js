bb69b7dfc08495ffd3aa96711eed09c593b76dcc
kmgarcia23 <kgarci83@calpoly.edu>
2021-04-10 21:35:06 -0700

    Linked table to front end

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
-import Form from './Form'
+import Form from './Form';
+import axios from 'axios';
 
 function MyApp() {
      const [characters, setCharacters] = useState([]);
@@ -11,9 +12,30 @@ function MyApp() {
         });
         setCharacters(updated);
         }
+
     function updateList(person){
         setCharacters([...characters,person]);
     }
+
+    async function fetchAll(){
+        try {
+           const response = await axios.get('http://localhost:5000/users');
+           return response.data.users_list;     
+        }
+        catch (error){
+           //We're not handling errors. Just logging into the console.
+           console.log(error); 
+           return false;         
+        }
+     }
+
+     useEffect(() => {
+        fetchAll().then( result => {
+           if (result)
+              setCharacters(result);
+         });
+     }, [] );
+
     return(
         <div className="container">
             <Table characterData={characters} removeCharacter={removeOneCharacter}/>

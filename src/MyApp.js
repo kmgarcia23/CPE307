b4345f4c9f91bdff613ec23dc70100e137640894
kmgarcia23 <kgarci83@calpoly.edu>
2021-04-11 23:04:21 -0700

    Added ID column to front end, update DELETE call

--- src/MyApp.js
+++ src/MyApp.js
@@ -4,58 +4,84 @@ import Form from './Form';
 import axios from 'axios';
 
 function MyApp() {
-     const [characters, setCharacters] = useState([]);
+   const [characters, setCharacters] = useState([]);
+   
+   async function fetchAll(){
+      try {
+         const response = await axios.get('http://localhost:5000/users');
+         return response.data.users_list;     
+      }
+      catch (error){
+         //We're not handling errors. Just logging into the console.
+         console.log(error); 
+         return false;         
+      }
+   }
 
-    function removeOneCharacter(index){
-        const updated = characters.filter((character,i) => {
+   async function makePostCall(person){
+      try {
+         const response = await axios.post('http://localhost:5000/users', person);
+         if(response.status === 201){
+            return response.data;
+         }
+         else{
+            return false;
+         }
+      }
+      catch (error) {
+         console.log(error);
+         return false;
+      }
+   }
+
+   function removeOneCharacter(index){
+      makeDeleteCall(characters[index]).then(result => {
+      if (result)
+      {
+         const updated = characters.filter((character,i) => {
             return i !== index
         });
-        setCharacters(updated);
-        }
+      setCharacters(updated);
+      }
+      });
+   }
 
-    async function fetchAll(){
-        try {
-           const response = await axios.get('http://localhost:5000/users');
-           return response.data.users_list;     
-        }
-        catch (error){
-           //We're not handling errors. Just logging into the console.
-           console.log(error); 
-           return false;         
-        }
-     }
+   async function makeDeleteCall(person){
+      try {
+         const response = await axios.delete('http://localhost:5000/users/'+person.id);
+         if(response.status === 204){
+            return response;
+         }
+         else{
+            return false;
+         }
+      }
+      catch (error) {
+         console.log(error);
+         return false;
+      }
+   }
 
-     async function makePostCall(person){
-        try {
-           const response = await axios.post('http://localhost:5000/users', person);
-           return response;
-        }
-        catch (error) {
-           console.log(error);
-           return false;
-        }
-     }
+   function updateList(person) { 
+      makePostCall(person).then( result => {
+      if (result)
+         setCharacters([...characters, result] );
+      });
+   }
 
-     function updateList(person) { 
-        makePostCall(person).then( result => {
-        if (result)
-           setCharacters([...characters, person] );
-        });
-     }
+   useEffect(() => {
+      fetchAll().then( result => {
+         if (result)
+            setCharacters(result);
+      });
+   }, [] );
 
-     useEffect(() => {
-        fetchAll().then( result => {
-           if (result)
-              setCharacters(result);
-         });
-     }, [] );
-
-    return(
-        <div className="container">
-            <Table characterData={characters} removeCharacter={removeOneCharacter}/>
-            <Form handleSubmit={updateList}/>
-        </div>
-    )
+   return(
+      <div className="container">
+         <Table characterData={characters} removeCharacter={removeOneCharacter}/>
+         <Form handleSubmit={updateList}/>
+      </div>
+   )
 }
 
 export default MyApp;
